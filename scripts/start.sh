550fdf7b7f4606a4ca85aaae157c97380641c0fc
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2016-06-24 19:57:25 +0200

    latest ubuntu based built (1.9.12)

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,12 +5,6 @@
 mkdir -p -m 0700 /root/.ssh
 echo -e "Host *\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
 
-if [ ! -z "$SSH_KEY" ]; then
- echo $SSH_KEY > /root/.ssh/id_rsa.base64
- base64 -d /root/.ssh/id_rsa.base64 > /root/.ssh/id_rsa
- chmod 600 /root/.ssh/id_rsa
-fi
-
 # Setup git variables
 if [ ! -z "$GIT_EMAIL" ]; then
  git config --global user.email "$GIT_EMAIL"
@@ -20,24 +14,33 @@ if [ ! -z "$GIT_NAME" ]; then
  git config --global push.default simple
 fi
 
-# Pull down code from git for our site!
+# Install Extras
+if [ ! -z "$DEBS" ]; then
+ apt-get update
+ apt-get install -y $DEBS
+fi
+
+# Pull down code form git for our site!
 if [ ! -z "$GIT_REPO" ]; then
-  rm -Rf /var/www/html
-  mkdir /var/www/html
+  rm /usr/share/nginx/html/*
   if [ ! -z "$GIT_BRANCH" ]; then
-    git clone -b $GIT_BRANCH $GIT_REPO /var/www/html/
+    git clone -b $GIT_BRANCH $GIT_REPO /usr/share/nginx/html/
   else
-    git clone $GIT_REPO /var/www/html/
+    git clone $GIT_REPO /usr/share/nginx/html/
   fi
-  chown -Rf nginx.nginx /var/www/html
+  chown -Rf nginx.nginx /usr/share/nginx/*
 fi
 
 # Display PHP error's or not
 if [[ "$ERRORS" != "1" ]] ; then
- echo php_flag[display_errors] = off >> /etc/php5/php-fpm.conf
-else
- echo php_flag[display_errors] = on >> /etc/php5/php-fpm.conf
+  sed -i -e "s/error_reporting =.*=/error_reporting = E_ALL/g" /etc/php5/fpm/php.ini
+  sed -i -e "s/display_errors =.*/display_errors = On/g" /etc/php5/fpm/php.ini
 fi
+
+# Tweak nginx to match the workers to cpu's
+
+procs=$(cat /proc/cpuinfo |grep processor | wc -l)
+sed -i -e "s/worker_processes 5/worker_processes $procs/" /etc/nginx/nginx.conf
 
 # Very dirty hack to replace variables in code with ENVIRONMENT values
 if [[ "$TEMPLATE_NGINX_HTML" == "1" ]] ; then
@@ -47,10 +50,13 @@ if [[ "$TEMPLATE_NGINX_HTML" == "1" ]] ; then
     value=$(echo "$i" | cut -d'=' -f2)
     if [[ "$variable" != '%s' ]] ; then
       replace='\$\$_'${variable}'_\$\$'
-      find /var/www/html -type f -exec sed -i -e 's/'${replace}'/'${value}'/g' {} \;
+      find /usr/share/nginx/html -type f -exec sed -i -e 's/'${replace}'/'${value}'/g' {} \;
     fi
   done
 fi
+
+# Again set the right permissions (needed when mounting from a volume)
+chown -Rf www-data.www-data /usr/share/nginx/html/
 
 # Start supervisord and services
 /usr/bin/supervisord -n -c /etc/supervisord.conf
